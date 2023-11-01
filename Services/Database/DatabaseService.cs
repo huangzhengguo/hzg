e9626379a3b9d13a415b139d89d1712d1e70cc7c
zhengguo <hzgmymail@163.com>
2023-11-01 15:09:19 +0800

    完善
    完善数据库服务

--- Services/Database/DatabaseService.cs
+++ Services/Database/DatabaseService.cs
@@ -7,10 +7,10 @@ namespace Hzg.Services;
 /// <summary>
 /// 数据库服务扩展
 /// </summary>
-public static class MySqlServerServiceExtesion
+public static class DatabaseServiceExtesion
 {
     /// <summary>
-    /// 添加 MySqlServer 服务
+    /// 添加 MySql 服务
     /// </summary>
     /// <param name="services">服务</param>
     /// <param name="configuration">配置</param>
@@ -24,4 +24,19 @@ public static class MySqlServerServiceExtesion
             options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         });
     }
+
+    /// <summary>
+    /// 添加 SqlServer 服务
+    /// </summary>
+    /// <param name="services">服务</param>
+    /// <param name="configuration">配置</param>
+    /// <param name="connectStr">连接字符串</param>
+    /// <typeparam name="T">数据库上下文类型</typeparam>
+    // static public void AddSqlServerService<T>(this IServiceCollection services, IConfiguration configuration, string connectStr) where T : DbContext
+    // {
+    //     services.AddDbContext<T>(options =>
+    //     {
+    //         options.UseSqlServer(configuration.GetConnectionString(connectStr));
+    //     });
+    // }
 }
